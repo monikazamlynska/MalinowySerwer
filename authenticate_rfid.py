e22e6288719134a52695879c536423ccc884490c
Monika <monika.zamlynska@gmail.com>
2022-08-19 08:01:42 +0200

    Dodanie logowania - proba

--- authenticate_rfid.py
+++ authenticate_rfid.py
@@ -32,7 +32,7 @@ try:
 		# dodawanie logu do pliku
 		plik.write("\n")
 		plik.write(datetime.datetime.now().strftime("[%Y-%m-%d %H:%M]     "))
-		plik.write("AUTORYZACJA:    <")
+		plik.write("AUTORYZACJA:     <")
 		plik.write(str(id))
 		plik.write(">: ")
 		plik.write(text)
