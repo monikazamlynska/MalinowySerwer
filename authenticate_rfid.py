e66a72c6f8358ebc02ba034c3359bc17e6a94ddb
Monika <monika.zamlynska@gmail.com>
2022-08-19 07:53:07 +0200

    Dodanie logowania - proba

--- authenticate_rfid.py
+++ authenticate_rfid.py
@@ -30,7 +30,6 @@ try:
 		print("Witaj ", text,  "! Masz uprawnienia na wejscie do serwerowni")
 
 		# dodawanie logu do pliku
-		plik.write("\n")
 		plik.write(datetime.datetime.now().strftime("[%Y-%m-%d %H:%M]     "))
 		plik.write("AUTORYZACJA:    <")
 		plik.write(str(id))
@@ -42,8 +41,7 @@ try:
 		GPIO.output(10, GPIO.HIGH) # jezeli nie, to zapal czerwona diode LED
 		print("Niestety nie masz uprawnien na wejscie do serwerowni!!! Skontaktuj się z Administratorem")
 
-		# dodawanie logu do pliku
-		plik.write("\n")
+		# dodawanie logu do pliku:
 		plik.write(datetime.datetime.now().strftime("[%Y-%m-%d %H:%M]     "))
 		plik.write("NIEAUTORYZOWANE: <")
 		plik.write(str(id))
