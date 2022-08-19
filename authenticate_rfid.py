13556f31092efa1274b57d6191f9628474971fa1
Monika <monika.zamlynska@gmail.com>
2022-08-19 07:21:53 +0200

    Dodanie logowania - proba

--- authenticate_rfid.py
+++ authenticate_rfid.py
@@ -27,7 +27,10 @@ try:
 	if(id == 876685318415): # jezeli ID karty wynosi tyle co podane, to zapal zielona diode LED
 		GPIO.output(8, GPIO.HIGH)
 		print("Witaj ", text,  "! Masz uprawnienia na wejscie do serwerowni")
-		plik.write(text," ",id,"/n")
+		plik.write(text)
+		plik.write(": ")
+		plik.write(id)
+		plik.write("/n")
 		sleep(1)
 	else:
 		GPIO.output(10, GPIO.HIGH) # jezeli nie, to zapal czerwona diode LED
